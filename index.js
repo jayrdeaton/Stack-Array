58ad392e7b8146f35546caae3896249805ab372d
jaydeaton <jayrdeaton@gmail.com>
2019-08-09 23:41:38 -0500

    return object at index if provided

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-module.exports = () => {
+const skip = 2
+
+module.exports = (n) => {
   const array = []
   const { stack } = new Error()
   const lines = stack.split('\n')
   // line 0 is error, line 1 is this function
-  for (let i = 2; i < lines.length; i++) {
+  for (let i = skip; i < lines.length; i++) {
     const line = lines[i].replace('at ', '').trim()
     const method = line.substring(0, line.indexOf(' '))
     const location = line.substring(line.indexOf('(') + 1, line.indexOf(')')).split(':')
-    array.push({
+    const obj = {
       method,
       path: location[0],
       line: location[1],
       column: location[2]
-    })
+    }
+    if (n === i - skip) return obj
+    array.push(obj)
   }
   return array
 }
